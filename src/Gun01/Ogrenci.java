00cb0cf850f2b322358ea0f8d0a842578bfa2932
trgyd <trgydmr17@gmail.com>
2023-01-25 21:49:36 +0300

    ikinci :) görevimi yaptım

--- src/Gun01/Ogrenci.java
+++ src/Gun01/Ogrenci.java
@@ -3,6 +3,10 @@ package Gun01;
 public class Ogrenci {
     public static void main(String[] args) {
 
-        
+        for (int i = 0; i < 5; i++) {
+            System.out.println("merhaba");
+
+        }
+
     }
 }
